7811ced7317bc17434f3bf93e7343c61a1c44793
DeviAyyagari <akdevi.92@gmail.com>
2021-09-09 15:43:16 -0700

    Migrate files other than images from EB's repo

---
+++ wedding/scripts/main.js
@@ -0,0 +1,132 @@
+// Add your javascript here
+// Don't forget to add it into respective layouts where this js file is needed
+$(document).ready(function() {
+  AOS.init({
+    // uncomment below for on-scroll animations to played only once
+    // once: true
+  }); // initialize animate on scroll library
+});
+
+function functionAlert(msg, myYes) {
+  var confirmBox = $("#confirm");
+  confirmBox.find(".message").text(msg);
+  confirmBox.find(".yes").unbind().click(function() {
+     confirmBox.hide();
+  });
+  confirmBox.find(".yes").click(myYes);
+  confirmBox.show();
+}
+
+$.urlParam = function(name){
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results==null) {
+     return null;
+  }
+  return decodeURI(results[1]) || 0;
+}
+
+setTimeout(function() {
+  $("#namediv").remove();
+}, 3000);
+
+$(document).ready(function() {
+  // let searchParams = new URLSearchParams(window.location.search)
+  // let param = searchParams.get('sent')
+  let name = $.urlParam('name')
+  $("#name").text("Vanakkam "+name+"! We are thrilled to invite you to celebrate the union of")
+  //$("#name").text("Hello "+window.location.href.substr(window.location.href.indexOf("?name=")+6));
+});
+// if (window.location.href.indexOf("?name=") > -1) {
+//   //functionAlert("Hey","");
+//   //$("#name).text = "Hello "+window.location.href.substr(window.location.href.indexOf("?name=")+6))
+
+//   $("#name").text("Hello "+window.location.href.substr(window.location.href.indexOf("?name=")+6));
+// }
+
+// Smooth scroll for links with hashes
+$("a.smooth-scroll").click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000,
+        function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }
+        }
+      );
+    }
+  }
+});
+
+// Photo Filter
+var activeFilter = "all";
+
+$(".ww-filter-button").on("click", function(e) {
+  // remove btn-primary from all buttons first
+  $(".ww-filter-button").removeClass("btn-primary");
+  $(".ww-filter-button").addClass("btn-outline-primary");
+
+  // add btn-primary to active button
+  var button = $(this);
+  button.removeClass("btn-outline-primary");
+  button.addClass("btn-primary");
+  filterItems(button.data("filter"));
+  e.preventDefault();
+});
+
+function filterItems(filter) {
+  if (filter === activeFilter) {
+    return;
+  }
+
+  activeFilter = filter;
+  $(".ww-gallery .card").each(function() {
+    var card = $(this);
+    var groups = card.data("groups");
+    var show = false;
+    if (filter === "all") {
+      show = true;
+    } else {
+      for (var i = 0; i < groups.length; i++) {
+        if (groups[i] === filter) {
+          show = true;
+        }
+      }
+    }
+    // hide everything first
+    card.fadeOut(400);
+    setTimeout(function() {
+      if (show && !card.is(":visible")) {
+        card.fadeIn(400);
+      }
+    }, 500);
+  });
+}
+
+// Light Box
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
